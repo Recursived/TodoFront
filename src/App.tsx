92186fba73832452dc799daf7fa90b4f718d4ac3
Alexandre MANETA <alex.m94430@gmail.com>
2023-02-28 00:24:26 +0100

    Adding state managament : step 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import {useState} from 'react';
 import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
@@ -6,43 +7,30 @@ import {TodoList} from './components/TodoItem/TodoList';
 import {TodoModel} from './models/TodoModel';
 
 function App() {
-	const todos : TodoModel[] = [
+	const [todos, setTodos] = useState<TodoModel[]>([
 		{
-			id:"1",
+			id: "1",
 			title: "Do the dishes",
 			completed: false
-		},
-		{
-			id:"2",
-			title: "Walk the dog",
-			completed: false
-		},
-		{
-			id:"3",
-			title: "Buy gift for the birthday",
-			completed: false
-		},
-		{
-			id:"4",
-			title: "Buy groceries",
-			completed: true
-		},
-	]
+		}
+	]);
 	return (
 		<section className='todoapp'>
-			<Header/>
-			<section className='main'>
-				<input id="toggle-all" className='toggle-all' type="checkbox" />
-				<label htmlFor='toggle-all'>Mark as all complete</label>
-				<TodoList>
-					{
-					todos.map((t, index) => 
-						<TodoItem key={index} {...t}/>
-					)
-					}
-				</TodoList>
-			</section>
-			<Footer/>
+			<Header />
+			{todos.length == 0 ? <></> : (
+				<section className='main'>
+					<input id="toggle-all" className='toggle-all' type="checkbox" />
+					<label htmlFor='toggle-all'>Mark as all complete</label>
+					<TodoList>
+						{
+							todos.map((t, index) =>
+								<TodoItem key={index} {...t} />
+							)
+						}
+					</TodoList>
+				</section>
+			)}
+			{todos.length == 0 ? <></> : <Footer />}
 		</section>
 	);
 }
